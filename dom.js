15582fdd27414d452f7b81de709d6b803794d608
kumarthakurneeraj123 <100923765+kumarthakurneeraj123@users.noreply.github.com>
2022-03-06 19:30:26 +0530

    Dom3

---
+++ dom.js
@@ -0,0 +1,22 @@
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = "123";
+// console.log(document.body);
+// console.log(document.head)
+// console.log(document.forms)
+// console.log(document.links);
+// console.log(document.all);
+// console.log(document.all[9]);
+// document.all[9].textContent="Hello";
+// console.log(document.doctype);
+// console.log(document.images);
+// var v = document.getElementById("main-header");
+
+// // v.innerHTML = '<h3>Bye</h3>';
+// // console.log(v);
+// v.style.borderBottom ='solid 3px #000';
+var item = document.getElementsByClassName('title');
+item[0].style.backgroundColor='green';
+console.log(item[0].innerText.fontcolor="red");
