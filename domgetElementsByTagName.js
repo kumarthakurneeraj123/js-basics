3e7443f4fb08eed2cc6652aa5575f6aaa9770482
kumarthakurneeraj123 <100923765+kumarthakurneeraj123@users.noreply.github.com>
2022-03-07 05:55:49 +0530

    getElementByTagName

---
+++ domgetElementsByTagName.js
@@ -0,0 +1,6 @@
+var item = document.getElementsByClassName('item2');
+console.log(item);
+item[0].style.backgroundColor = 'pink';
+var itemByTag = document.getElementsByTagName('li');
+console.log(itemByTag);
+itemByTag[4].style.fontWeight = 'bold';
